21b244d8a728ed81598b317d04c7a361d1d25dd7
walefy <walefyd1@gmail.com>
2024-12-08 21:39:55 -0300

    day 04 part 02

---
+++ ts/challenge04-part02.ts
@@ -0,0 +1,47 @@
+const MAS = 'MAS';
+const RMAS = 'SAM';
+
+const isMas = (text: string) => text === MAS || text === RMAS;
+
+function formatMatrix(baseString: string) {
+  return baseString.split('\n').map((row) => row.split(''));
+}
+
+function countXmasDiagonal(
+  position: number,
+  rowPosition: number,
+  matrix: Array<Array<string>>,
+): number {
+  const topRow = matrix[rowPosition - 1];
+  const row = matrix[rowPosition];
+  const bottomRow = matrix[rowPosition + 1];
+
+  if (!topRow || !row || !bottomRow) return 0;
+  if (row[position] !== 'A') return 0;
+
+  const mas1 = topRow[position - 1] + 'A' + bottomRow[position + 1];
+  const mas2 = topRow[position + 1] + 'A' + bottomRow[position - 1];
+
+  if (isMas(mas1) && isMas(mas2)) return 1;
+
+  return 0;
+}
+
+function main() {
+  const inputString = Deno.readTextFileSync('inputs/input04.txt');
+  const matrix = formatMatrix(inputString);
+
+  let xmasCount = 0;
+
+  for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+    const row = matrix[rowIndex];
+
+    for (let letterIndex = 0; letterIndex < row.length; letterIndex++) {
+      xmasCount += countXmasDiagonal(letterIndex, rowIndex, matrix);
+    }
+  }
+
+  console.log(xmasCount);
+}
+
+main();
